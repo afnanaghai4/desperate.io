5d7137d54cdbcff2740bda93c442f3121a08eb97
afnanaghai4 <afnanaghai4@gmail.com>
2026-04-09 20:50:36 +0200

    made create-job API with proper authentication and added functionality of uploading either job LINK or job TEXT for the description

--- backend/src/jobs/job.controller.ts
+++ backend/src/jobs/job.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
-@Controller('job')
-export class JobController {}
+import { Request } from 'express';
+import { CreateJobDto } from './dto/create-job.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Job } from 'src/entities/job.entity';
+import { JobService } from './job.service';
+
+interface AuthRequest extends Request {
+  user: { userId: number; email: string };
+}
+
+@Controller('jobs')
+export class JobController {
+  constructor(private readonly jobService: JobService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  async createJob(
+    @Body() createJobDto: CreateJobDto,
+    @Req() req: AuthRequest,
+  ): Promise<{ message: string; data: Job }> {
+    const job = await this.jobService.createJob(createJobDto, req.user.userId);
+    return {
+      message: 'Job created successfully',
+      data: job,
+    };
+  }
+}
--- backend/src/jobs/job.module.ts
+++ backend/src/jobs/job.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Job } from 'src/entities/job.entity';
+import { UsersModule } from '../users/users.module';
 import { JobController } from './job.controller';
 import { JobService } from './job.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job]), UsersModule],
   controllers: [JobController],
   providers: [JobService],
   exports: [JobService],
